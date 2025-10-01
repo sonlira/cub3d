aca9a06ed7685c55045fa6051c8268a35be55683
Ari <aril.abcmail@gmail.com>
2025-10-01 21:04:56 +0200

    update main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 15:29:13 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/09/22 13:16:34 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/10/01 19:25:24 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,7 @@
 int	main(int argc, char **argv)
 {
 	t_game	game;
-	int		fd;
 
-	requiere_valid_program_args(argc, argv);
-	requiere_valid_file(argv[1]);
-	open_file(argv[1], &fd);
-	config_parser(&game, fd);
-	map_loader(&game, fd);
+	game_parser(argc, argv, &game);
 	return (0);
 }
