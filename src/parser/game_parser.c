9dc360f90545c668dc4c9b3cc084b446519718d6
Ari <aril.abcmail@gmail.com>
2025-10-03 20:20:46 +0200

    Update

--- src/parser/game_parser.c
+++ src/parser/game_parser.c
@@ -6,13 +6,13 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 20:41:54 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/10/03 14:14:12 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/10/03 20:20:10 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	t_game_init(t_game *game)
+static void	game_init(t_game *game)
 {
 	ft_bzero(game, sizeof(*game));
 }
@@ -21,7 +21,7 @@ void	game_parser(int argc, char **argv, t_game *game)
 {
 	int	fd;
 
-	t_game_init(game);
+	game_init(game);
 	require_valid_program_args(argc, argv);
 	require_valid_file(argv[1]);
 	open_file(argv[1], &fd);
