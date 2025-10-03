7d4fc925e1d1c30bb8ca73e00a504c98cf791eaf
Ari <aril.abcmail@gmail.com>
2025-10-03 20:16:33 +0200

    Se cambio el nombre de t_player_init a player_init y se mavio a player/

--- src/parser/game_parser.c
+++ src/parser/game_parser.c
@@ -6,7 +6,7 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 20:41:54 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/10/01 20:55:05 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/10/03 14:14:12 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ static void	t_game_init(t_game *game)
 	ft_bzero(game, sizeof(*game));
 }
 
-static void	t_player_init(t_map *map, t_player *player)
-{
-	ft_bzero(player, sizeof(*player)); // aseguras estado limpio
-	find_player(map->grid, player); // llena x, y, dir
-}
 void	game_parser(int argc, char **argv, t_game *game)
 {
 	int	fd;
@@ -32,8 +27,5 @@ void	game_parser(int argc, char **argv, t_game *game)
 	open_file(argv[1], &fd);
 	config_parser(game, fd);
 	map_parser(game, fd);
-	t_player_init(game->map, &game->player);
 	close(fd);
-	// ft_print_str_array(game->map->grid);
-	// exit_error_and_free(game, "Fuera de game\n");
 }
